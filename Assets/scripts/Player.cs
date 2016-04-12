d1c93f86205b785560fde08931edada38ca4f13b
harishskytou <harish@skytousolution.com>
2016-04-12 19:23:20 +0530

    Made logic to move left and right enemy when ride on opposite side

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -5,24 +5,56 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour {
 
 
-    public float moveSpeed=5;
+    public float moveSpeed;
+	public GameObject centerEnemy, leftEnemy, rightEnemy;
     private float currentMovementValueX,currentMovementValueY;
+	public float  leftDistance, centerDistance, rightDistance;
+	public float enemyStartPosZ, rideDist, enemyMoveSpeed;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
 
+	void Update () 
+	{
         currentMovementValueX = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        currentMovementValueY = CrossPlatformInputManager.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-
-      //  Debug.Log("Move value : " + currentMovementValue);
-
+		currentMovementValueY = CrossPlatformInputManager.GetAxis ("Vertical") * moveSpeed * Time.deltaTime;
         this.transform.Translate(currentMovementValueX, 0, currentMovementValueY);
-
-
+		CalculateDistance ();
+		AdvaceCover ();
     }
+
+	void CalculateDistance()
+	{
+		centerDistance = Vector3.Distance (transform.position, centerEnemy.transform.position);
+		leftDistance = Vector3.Distance (transform.position, leftEnemy.transform.position);
+		rightDistance = Vector3.Distance (transform.position, rightEnemy.transform.position);
+	}
+
+	void AdvaceCover()
+	{
+		if ((leftDistance <rideDist) && (rightEnemy.transform.position.z >rideDist))
+		{
+			rightEnemy.transform.Translate (0, 0, -enemyMoveSpeed);
+		} 
+
+		else if((leftDistance > rideDist) && (rightEnemy.transform.position.z < enemyStartPosZ )) 
+		{
+			rightEnemy.transform.Translate (0, 0,enemyMoveSpeed);
+		} 
+
+		if ((rightDistance < rideDist) && (leftEnemy.transform.position.z >rideDist))
+		{
+			leftEnemy.transform.Translate (0, 0, -enemyMoveSpeed);
+		} 
+
+		else if((rightDistance > rideDist) && (leftEnemy.transform.position.z < enemyStartPosZ )) 
+		{
+			leftEnemy.transform.Translate (0, 0,enemyMoveSpeed);
+		} 
+	}
 }
+
