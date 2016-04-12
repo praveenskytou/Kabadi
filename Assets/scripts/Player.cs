be6c898243bf7ae3fddb63362a1cfd185319c030
Praveen <praveen@skytousolution.com>
2016-04-12 16:32:28 +0530

    Added player movement script.

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+
+    public float moveSpeed=5;
+    private float currentMovementValueX,currentMovementValueY;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        currentMovementValueX = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        currentMovementValueY = CrossPlatformInputManager.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+      //  Debug.Log("Move value : " + currentMovementValue);
+
+        this.transform.Translate(currentMovementValueX, 0, currentMovementValueY);
+
+
+    }
+}
