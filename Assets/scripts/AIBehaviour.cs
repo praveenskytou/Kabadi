d60f909bd83f1f0f0bbd360ff2f5d6d5e09c7f36
Praveen <praveen@skytousolution.com>
2016-04-13 19:06:13 +0530

    Some updates on AI

--- Assets/scripts/AIBehaviour.cs
+++ Assets/scripts/AIBehaviour.cs
@@ -8,18 +8,26 @@ public enum AIStates
     attack,
     defence
 }
+
 public class AIBehaviour : MonoBehaviour
 {
 
-    public AIBehaviour instance = null;
+    AIBehaviour instance = null;
 
     public GameObject playerRef;
     public AIStates aiStates;
+
+    //ai params
+    private Vector3 startPosition;
+    public float aiMovementTriggerDistance, aiSafeDistance;
+    public float aiMoveSpeed;
+    private float moveStep;
 
     void Awake()
     {
         instance = this;
         playerRef = GameObject.FindGameObjectWithTag("Player");
+        startPosition = this.transform.position;
     }
 
 	// Use this for initialization
@@ -36,13 +44,42 @@ public class AIBehaviour : MonoBehaviour
 
     void Attack()
     {
+        if(Vector2.Distance(playerRef.transform.position, this.transform.position) < aiMovementTriggerDistance )
+        {
 
+            if (Vector2.Distance(playerRef.transform.position, this.transform.position) > aiSafeDistance)
+            {
+                moveStep = aiMoveSpeed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, playerRef.transform.position, moveStep);
+            }
+            else
+            {
+                moveStep = aiMoveSpeed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(new Vector3(transform.position.x,0,0), new Vector3(playerRef.transform.position.x, 0, 0), moveStep);
+            }
+
+        }
+        //else
+        //if(Vector2.Distance(playerRef.transform.position, this.transform.position) < aiSafeDistance)
+        //{
+        //    moveStep = aiMoveSpeed * Time.deltaTime;
+        //    transform.position = Vector3.MoveTowards(new Vector3(transform.position.x,0,0), new Vector3(playerRef.transform.position.x, 0, 0), moveStep);
+        //}
+        //else
+
+        //retreat
+        if (Vector2.Distance(playerRef.transform.position, this.transform.position) > aiMovementTriggerDistance ) 
+        {
+            moveStep = aiMoveSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, startPosition, moveStep);
+        }
     }
 
 	// Update is called once per frame
 	void Update ()
     {
 	
+
         switch(aiStates)
         {
             case AIStates.idle:
