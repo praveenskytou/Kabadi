4ff908a56b59cb8e1fd08b8de92d563255d40af7
Praveen <praveen@skytousolution.com>
2016-04-13 13:13:32 +0530

    code basic structure for AI

---
+++ Assets/scripts/AIBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public enum AIStates
+{
+    idle, 
+    attack,
+    defence
+}
+public class AIBehaviour : MonoBehaviour
+{
+
+    public AIBehaviour instance = null;
+
+    public GameObject playerRef;
+    public AIStates aiStates;
+
+    void Awake()
+    {
+        instance = this;
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+    }
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+
+    void Idle()
+    {
+
+    }
+
+    void Attack()
+    {
+
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+	
+        switch(aiStates)
+        {
+            case AIStates.idle:
+
+                Idle();
+                break;
+
+            case AIStates.attack:
+
+                Attack();
+
+                break;
+        }
+	}
+}
