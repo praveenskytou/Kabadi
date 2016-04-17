46f37ffe7aaccfa890d34276f3f4ea71e880ad7f
Praveen <praveen@skytousolution.com>
2016-04-17 17:56:26 +0530

    Updated with evasion and added one more AI.

--- Assets/scripts/AIBehaviour.cs
+++ Assets/scripts/AIBehaviour.cs
@@ -12,7 +12,8 @@ public enum AIStates
 public enum AIEvadeDirection
 {
     left,
-    right
+    right,
+
 }
 
 public class AIBehaviour : MonoBehaviour
@@ -21,14 +22,25 @@ public class AIBehaviour : MonoBehaviour
     AIBehaviour instance = null;
 
     public GameObject playerRef;
+    private Vector2 attackPosition;
     public AIStates aiStates;
 
-    //ai params
+    //ai coords : params
     private Vector3 startPosition;
     public float aiSafeDistance;
     public float aiVerticalMoveSpeed, aiHorizontalMoveSpeed;
+    public float aiAttackSpeed;//it is always greater than normal speed
     private float xpos, ypos;
-    public AIEvadeDirection currentEvadeDirection;
+
+    //ai state change vars
+    private float timeElapsed_StateChange;
+    public float nextStateChangeDuration;//more than 5sec wud be gud
+
+    //ai evade vars
+    public AIEvadeDirection aiEvadeDir;
+    private float timeElapsed_Evade;
+    public float nextEvadeStartDuration;//more than 3sec wud be gud
+    public Vector3 evadeToPosition;
 
     //misc params
     private float moveStep;
@@ -36,74 +48,113 @@ public class AIBehaviour : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-	
-	}
-
-    void Idle()
-    {
-
+        timeElapsed_StateChange = Time.time;
+        timeElapsed_Evade = Time.time;
+        evadeToPosition = new Vector2(transform.position.x , transform.position.y);
     }
 
-
-    void Defense()
-    {
-
-        ////Evasion
-        //if (transform.position.x > playerRef.transform.position.x)
-        //    currentEvadeDirection = AIEvadeDirection.right;
-        //else
-        //if (transform.position.x < playerRef.transform.position.x)
-        //    currentEvadeDirection = AIEvadeDirection.left;
-
-
-        //If player comes near the AI, then evade
-        if (Vector2.Distance(playerRef.transform.position, this.transform.position) < aiSafeDistance)
-        {
-            moveStep = aiVerticalMoveSpeed * Time.deltaTime;
-
-            xpos = playerRef.transform.position.x + 0.2f;
-            ypos = playerRef.transform.position.y + aiSafeDistance;
-
-            //transform.position = Vector3.MoveTowards(transform.position, playerRef.transform.position, moveStep);
-        }
-        else
-        {
-            //If player moves away, then step forward
-
-
-        }
-
-
-
-        //movement 
-        transform.position = Vector3.MoveTowards(transform.position, new Vector2(xpos, ypos), moveStep);
-
-    }
-
-    void Attack()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
     {
         boundsCheck();
 
-        switch (aiStates)
+        //Evasion change logic
+        if (Time.time - timeElapsed_Evade > nextEvadeStartDuration)
         {
-            case AIStates.idle:
-                Idle();
-                break;
+            if (aiEvadeDir == AIEvadeDirection.right)
+            {
+                aiEvadeDir = AIEvadeDirection.left;
+                evadeToPosition = new Vector2(transform.position.x - 0.5f, transform.position.y);
+            }
+            else
+            {
+                aiEvadeDir = AIEvadeDirection.right;
+                evadeToPosition = new Vector2(transform.position.x + 0.5f, transform.position.y);
+            }
 
-            case AIStates.defense:
-                Defense();
-                break;
+            timeElapsed_Evade = Time.time;
+        }
+
+        performEvasion(evadeToPosition);
+
+
+        //State change logic
+        if (Time.time - timeElapsed_StateChange > nextStateChangeDuration)
+        {
+            if (aiStates == AIStates.defense)
+                aiStates = AIStates.attack;
+            else
+                aiStates = AIStates.defense;
+
+            attackPosition = playerRef.transform.position;//get the recent player pos
+
+            timeElapsed_StateChange = Time.time;
+        }
+
+
+        //If the player is nearer to AI
+        if (Vector2.Distance(playerRef.transform.position, this.transform.position) < aiSafeDistance)
+        {
+            switch (aiStates)
+            {
+                case AIStates.attack:
+                    Attack();
+                    break;
+
+                case AIStates.defense:
+                    Defense();
+                    break;
+            }
         }
     }
 
+    private void Defense()
+    {
 
-    void boundsCheck()
+        moveStep = aiVerticalMoveSpeed * Time.deltaTime;
+
+        xpos = playerRef.transform.position.x + 0.2f;
+        ypos = playerRef.transform.position.y + aiSafeDistance;
+
+        //movement 
+        transform.position = Vector3.MoveTowards(transform.position, new Vector2(xpos, ypos), moveStep);
+
+    }
+
+    private void Attack()
+    {
+
+        moveStep = aiAttackSpeed * Time.deltaTime;
+
+        transform.position = Vector3.MoveTowards(transform.position, attackPosition, moveStep);
+
+        if(Vector3.Distance(transform.position, attackPosition) < 0.2f)
+        {
+            //revert to defense state
+            aiStates = AIStates.defense;
+        }
+
+    }
+
+    private void performEvasion(Vector2 evadeToPosition)
+    {
+            //evade a bit
+            moveStep = 1 * Time.deltaTime;
+
+            transform.position = Vector3.MoveTowards(transform.position, evadeToPosition, moveStep);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player" && aiStates == AIStates.attack)
+        {
+            Debug.Log("AI caught the Player");
+        }
+    }
+
+    private void boundsCheck()
     {
 
         //Endline_back check
@@ -121,5 +172,7 @@ public class AIBehaviour : MonoBehaviour
 
 
     }
+
+
 
 }
