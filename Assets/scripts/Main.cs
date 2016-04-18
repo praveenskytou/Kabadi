4c3fa172de68f598fb0916eec9d9c19bee802cf8
Praveen <praveen@skytousolution.com>
2016-04-18 19:44:16 +0530

    Added main scene

---
+++ Assets/scripts/Main.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void load2DScene()
+    {
+        SceneManager.LoadScene("2dscene");
+    }
+
+    public void load3DScene()
+    {
+        SceneManager.LoadScene("3dscene");
+    }
+}
